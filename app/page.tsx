9de86a794acd0d183c50c8e2d62a724e43053560
shri29s <shricharan.r24@iiits.in>
2025-10-14 22:20:42 +0530

    Change background

--- app/page.tsx
+++ app/page.tsx
@@ -10,13 +10,22 @@ export default async function Home() {
   const session = await auth();
 
   return (
-    <div className="min-h-screen bg-gradient-to-br from-slate-900 via-blue-900 to-purple-900">
-      <Navigation session={session} />
-      <Hero />
-      <Features />
-      <About />
-      <CTA />
-      <Footer />
+    <div className="min-h-screen bg-gradient-to-br from-slate-900 via-blue-900 to-purple-900 relative">
+      {/* Background Image */}
+      <div
+        className="fixed inset-0 z-0 bg-cover bg-center bg-no-repeat opacity-60"
+        style={{
+          backgroundImage: "url('/images/background.png')",
+        }}
+      ></div>
+      <div className="relative z-10">
+        <Navigation session={session} />
+        <Hero />
+        <Features />
+        <About />
+        <CTA />
+        <Footer />
+      </div>
     </div>
   );
 }
